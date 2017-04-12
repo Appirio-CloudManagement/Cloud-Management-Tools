258e8cc348c55032592aa8e6bedd1e3f5dee2f94
guycurry <guy.curry@gmail.com>
2017-04-12 17:41:29 -0400

    fix css and js links in move from chrome ext to nodejs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-pp.engine('html', require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile);
 //app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
